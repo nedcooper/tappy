ae2e36c38b1e7816f41ce39a057c3cd96a966c2b
Edward Cooper <61486054+nedcpr@users.noreply.github.com>
2022-03-31 09:49:10 +1100

    Update Tap.py

--- Tap.py
+++ Tap.py
@@ -3,9 +3,12 @@
 import HuskyLens
 import Arduino
 
-ObjectID = HuskyLens.GetObjectID()  # Get the current object ID
 
-ObjectDictionary = {                # Maps to object identity from ID
+# Get the current object ID
+ObjectID = HuskyLens.GetObjectID()
+
+# Maps to object identity from ID
+ObjectDictionary = {
     0: "NONE",
     1: "ToothBrush",
     2: "Apple",
@@ -13,15 +16,17 @@ ObjectDictionary = {                # Maps to object identity from ID
     4: "Hand"
 }
 
-CurrentWaterVolume = [              # Current state of learned water volume. Read as: [percentage open, time open]
+# Current state of learned water volume. Read as: [percentage open, time open]
+CurrentWaterVolume = [
     [0,0],[20,5],[70,7],[60,7],[80,15]
 ]
 
-MinWaterVolume = [                  # Minimum water volume for each object. Read as: [percentage open, time open]
+# Minimum water volume for each object. Read as: [percentage open, time open]
+MinWaterVolume = [
     [0,0],[10,2],[40,5],[40,5],[70,15]
 ]
 
-# Function that updates and decrease water volume use if user doesn't use the full allocated volume
+# Function that updates (decrease) water volume allocation if user doesn't use the full allocated volume
 def UpdateWaterVolume(ObjectID, NewVolume): 
     CurrentWaterVolume(ObjectID) = min(MinWaterVolume(ObjectID), 0.2*NewVolume + 0.8*CurrentWaterVolume(ObjectID))
     return CurrentWaterVolume(ObjectID)
@@ -30,7 +35,7 @@ def UpdateWaterVolume(ObjectID, NewVolume):
 while ObjectID != 0:
     State = Arduino.RotateValve(CurrentWaterVolume(ObjectID))
 
-# If the users stop using the water to full allocated amount, close the tap and reduce the volume
+# If the user stops using the water prior to delivery of full allocated amount, close the tap and reduce the volume
 if ObjectID == 0 & State != 0:
     Arduino.CloseValve()
     CurrentWaterVolume(Arduino.LastObjectID) = UpdateWaterVolume(Arduino.LastObjectID, Arduino.LastObjectVolume)
